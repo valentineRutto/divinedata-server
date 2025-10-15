0878ae2032c3ba8e3ccc01745987ac07c38930fd
valentinerutto <valentinrutto@gmail.com>
2025-10-15 16:27:55 +0300

    set up server arch

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "divinedata-server"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/routes/Routing.kt
@@ -0,0 +1,19 @@
+package com.example.com.routes
+
+import io.ktor.http.ContentType
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+    }
+}
