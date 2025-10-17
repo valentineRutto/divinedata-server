6df2b9d8408c9546ef11908bcb0a6c1d64b38c48
valentinerutto <valentinrutto@gmail.com>
2025-10-17 23:48:35 +0300

    -setup monitoring

--- src/main/kotlin/Monitoring.kt
+++ src/main/kotlin/Monitoring.kt
@@ -1,22 +1,14 @@
 package com.example.com
 
-import io.ktor.serialization.gson.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
 import io.ktor.server.request.path
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import org.slf4j.event.*
 import kotlin.text.startsWith
 
 fun Application.configureMonitoring() {
-
     install(CallLogging) {
-
         CallLoggingConfig().level = Level.INFO
         CallLoggingConfig().filter { call -> call.request.path().startsWith("/") }
 
--- src/main/kotlin/routes/Routing.kt
+++ src/main/kotlin/routes/Routing.kt
@@ -10,6 +10,7 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+
         get("/test1") {
             val text = "<h1>Hello From Ktor</h1>"
             val type = ContentType.parse("text/html")
